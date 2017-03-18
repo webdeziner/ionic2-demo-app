547fd35001651041523555b561b867d3ea898ed8
Victor Eliasson <victor.eliasson@webdeziner.se>
2017-03-18 14:47:56 +0100

    Update 3
    Begin SQLite page

--- src/pages/sqlite/sqlite.ts
+++ src/pages/sqlite/sqlite.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { SQLite } from 'ionic-native';
 
 @Component({
   selector: 'page-sqlite',
   templateUrl: 'sqlite.html'
 })
 export class SqlitePage {
+  db:any = new SQLite();
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SqlitePage');
+  }
+
+  ionViewDidEnter() {
+    this.load();
+  }
+
+  load() {
+
+  }
+
+  add() {
+
   }
 
 }
